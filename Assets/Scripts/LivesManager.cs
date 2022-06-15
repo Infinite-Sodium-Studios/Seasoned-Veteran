c8ca0faef0f856b3c9d352448ce23a975c597e49
s-nandi <saujas.nandi@gmail.com>
2022-06-14 21:07:13 -0700

    Implement player-enemy collision and lives manager

---
+++ Assets/Scripts/LivesManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class LivesManager : MonoBehaviour
+{
+    [SerializeField] private int initialLives;
+    private int currentLives;
+    void Start()
+    {
+        Debug.Assert(initialLives > 0);
+        currentLives = initialLives;
+    }
+
+    public int GetLives()
+    {
+        return Mathf.Max(0, currentLives);
+    }
+
+    // Update is called once per frame
+    public void HitEvent()
+    {
+        currentLives--;
+        if (currentLives == 0)
+        {
+            StateTransition.GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/StateTransition.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class StateTransition : MonoBehaviour
+{
+    public static void GameOver()
+    {
+        Debug.Log("Game over!");
+    }
+}
