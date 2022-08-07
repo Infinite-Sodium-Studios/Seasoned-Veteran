f6413e0363b5d43f23d2d43240d157acaa803b49
s-nandi <saujas.nandi@gmail.com>
2022-08-07 12:39:18 -0700

    Change enemy behavior to move towards target door

---
+++ Assets/Scripts/Enemy/EnemyTargetCollision.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EnemyTargetCollision : MonoBehaviour
+{
+    private PlayerScoreManager playerScoreManager;
+    [SerializeField] private GameStateManager gameManager;
+
+    void Start()
+    {
+        playerScoreManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScoreManager>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.gameObject.CompareTag("Enemy"))
+        {
+            return;
+        }
+        Destroy(collider.gameObject);
+        Debug.Assert(playerScoreManager != null, "PlayerScoreManager cannot be null");
+        playerScoreManager.OnEnemyEscape();
+        if (playerScoreManager.GetEnemiesEscapedSlack() <= 0)
+        {
+            gameManager.GameOver();
+        }
+    }
+}
